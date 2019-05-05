cbe00a1693fc1a75ac92e8287ba8eb9c1eea708c
lkasum1 <lkasum1@etf.unsa.ba>
2019-05-05 14:07:00 +0200

    Ruta za listu odslusanih predmeta

--- Routes/predmeti.js
+++ Routes/predmeti.js
@@ -49,4 +49,26 @@ router.get('/trenutni/:idStudent', function (req, res, next) {
 
 });
 
+router.get('/odslusani/:idStudent', function (req, res, next) {
+
+    var trenutna_id = 0;
+    const student_id = req.params.idStudent;
+
+    //Odredjivanje trenutne godine
+
+    db.AkademskaGodina.findOne({
+        where: {
+            aktuelna: 1
+        },
+        attributes: ['id']
+    }).then(trenutna => {
+        console.log(trenutna.id);
+        db.sequelize.query("SELECT Predmet.naziv FROM Predmet, predmet_student WHERE Predmet.id=predmet_student.idPredmet AND predmet_student.idStudent=" + student_id + " AND idAkademskaGodina!=" + trenutna.id).then(([results, metadata]) => res.json({
+            data: results
+        }))
+
+    })
+
+});
+
 module.exports = router;
