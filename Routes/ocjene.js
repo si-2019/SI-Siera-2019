fc9017653bf8c18ac6fc8053cdfa788915846716
orucahmed <orucahmed@gmail.com>
2019-05-23 17:26:44 +0200

    Kreiranje APIa koji vraća ocjene po akademskoj godini, studentu i predmetu

--- Routes/ocjene.js
+++ Routes/ocjene.js
@@ -3,55 +3,51 @@ var router = express.Router();
 const db = require('../db.js');
 
 
-//Lista svih ocjena po akademskoj godini
-router.get('/akademskaGodina/:idGodina', function (req, res) {
-    const akademskaGodina_id = req.params.idGodina
-    db.predmet_student.findAll({
-        where:{
-            idAkademskaGodina:akademskaGodina_id
-        }
-    }).then(ocjene=>res.json({
-        error: false,
-        data: ocjene
-    })).catch(error=>res.json({
-        error: true,
-        data: [],
-        error:error
-    }));
-});
-
 //Lista svih ocjena po studentu
-router.get('/student/:idStudenta', function (req, res) {
-    const student_id = req.params.idStudenta
-    db.predmet_student.findAll({
-        where:{
-            idStudent:student_id
+router.get('/:idStudenta', function (req, res) {
+    
+    const studnet_id = req.params.idStudenta;
+
+
+    var ocjeneniz = []
+    var brojac=0;
+
+    db.sequelize.query("SELECT DISTINCT predmet_student.idAkademskaGodina, AkademskaGodina.naziv FROM predmet_student, AkademskaGodina WHERE predmet_student.idStudent=" + studnet_id + " AND predmet_student.idAkademskaGodina = AkademskaGodina.id ORDER BY AkademskaGodina.naziv").then(([godine,metadata])=>{
+        for(var i=0; i<godine.length; i++){
+            
+            var ak_id=godine[i].idAkademskaGodina
+
+            
+
+            db.sequelize.query("SELECT Predmet.naziv AS Predmet, predmet_student.ocjena AS Ocjena FROM Predmet, predmet_student WHERE predmet_student.idStudent=" + studnet_id + " AND Predmet.id=predmet_student.idPredmet AND predmet_student.idAkademskaGodina="+ak_id).then(([ocjene,metadata])=>{
+
+                ocjeneniz.push ([{
+                    AkademskaGodina : godine[brojac].naziv,
+                    Ocjene : []
+                    
+                }]) 
+
+                for(var j=0; j<ocjene.length; j++){
+                    ocjeneniz[brojac][0].Ocjene.push({
+                        Predmet: ocjene[j].Predmet,
+                        Ocjena: ocjene[j].Ocjena
+                    })
+                }
+                brojac++
+                if(brojac==godine.length){
+                    res.status(200).json({
+                        ocjene: ocjeneniz
+                    })
+                }
+                
+            })
         }
-    }).then(ocjene=>res.json({
-        error: false,
-        data: ocjene
-    })).catch(error=>res.json({
-        error: true,
-        data: [],
-        error:error
-    }));
+
+        
+    })
+  
 });
 
-//Lista svih ocjena po predmetu
-router.get('/predmet/:idPredmeta', function (req, res) {
-    const predmet_id = req.params.idPredmeta
-    db.predmet_student.findAll({
-        where:{
-            idPredmet:predmet_id
-        }
-    }).then(ocjene=>res.json({
-        error: false,
-        data: ocjene
-    })).catch(error=>res.json({
-        error: true,
-        data: [],
-        error:error
-    }));
-});
+
 
 module.exports = router;
