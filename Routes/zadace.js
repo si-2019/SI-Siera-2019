bf2f5cbdfd59be474e4526457a79098fc8e9d52e
lkasum1 <lkasum1@etf.unsa.ba>
2019-06-11 18:22:11 +0200

    Ispravke neke dodane

--- Routes/zadace.js
+++ Routes/zadace.js
@@ -6,10 +6,10 @@ const bodyParser = require('body-parser');
 const assert = require('assert');
 
 router.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }))
 
-router.get('/:idStudent', function(req, res) {
+router.get('/:idStudent', function (req, res1) {
 
     const student_id = req.params.idStudent;
     var isp = [];
@@ -23,7 +23,7 @@ router.get('/:idStudent', function(req, res) {
         }
     }).then(broj => {
         if (broj == 0) {
-            return res.status(404).send({
+            return res1.status(404).send({
                 success: 'false',
                 message: 'Korisnik not found'
             });
@@ -31,56 +31,73 @@ router.get('/:idStudent', function(req, res) {
         else {
 
             //Slanje requesta da bi dobio predmete i bodove na zadace 
-            request('http://localhost:31918/zadace/' + student_id + '/sort', function(err, res, body){
-            
-            var data = JSON.parse(body);
-            if(data.ispiti.length == 0) {
-                return res.status(200).send({
-                    succes : 'true',
-                    message : 'Succesful',
-                    ZadacePoGodinamaIPredmetima : 'Student jos nema zadaca'
-                })
-            }
+            request('http://localhost:31918/zadace/' + student_id + '/sort', function (err, res, body) {
+
+                var data = JSON.parse(body);
+                if (!data.ispiti) {
+                    return res1.status(200).send({
+                        succes: 'true',
+                        message: 'Succesful',
+                        ZadacePoGodinamaIPredmetima: 'Student jos nema zadaca'
+                    })
+                }
+
+
 
                 // prolazak kroz predmete koje sam dobio u requ
-                for(var i = 0; i < data.ispiti.length; i++) {
+
+                for (var i = 0; i < data.ispiti.length; i++) {
+                    var pom = i;
+
                     p = false;
-                    for(var j = 0; j < isp.length; j++) {
-                        if(isp[j] == ispiti[i].naziv)
+                    //console.log("isp duzina:" + isp.length)
+                    for (var j = 0; j < isp.length; j++) {
+
+                        if (isp[j] == data.ispiti[i].naziv)
                             p = true;
                     }
-                //smjestanje u niz isp da bih nasao akademsku godinu za svaki od predmeta jednom
-                // p pomocna varijabla za provjeru da li se predmet nalazi u nizu
-                    if(p == false) {
+
+                    //smjestanje u niz isp da bih nasao akademsku godinu za svaki od predmeta jednom
+                    // p pomocna varijabla za provjeru da li se predmet nalazi u nizu
+                    if (p == false) {
                         isp.push(data.ispiti[i].naziv);
                         // Prolajaz kroz petlju da bi se nasla godina u kojoj student ima ovaj predmet
-                            db.sequelize.query("SELECT AkademskaGodina.naziv FROM Predmet, predmet_student, AkademskaGodina WHERE predmet_student.idStudent=" + student_id + " AND Predmet.id=predmet_student.idPredmet AND predmet_student.idAkademskaGodina=AkademskaGodina.id AND Predmet.naziv = " + data.ispiti[i].naziv).then(([godina, metadata]) => {
-                                //t pomocna varijabla koja govori da li se akademska godina vec nalazi u finalnom nizu
-                                t = false;
-                                for(var k = 0; k < niz.length; k++) {
-                                    // ako se vec nalazi u finalnom nizu updateuje se samo atribut zadace sa novim predmetom
-                                    if(niz[k].AkademskaGodina == godina[0].naziv) {
-                                        t = true;
-                                        niz[k].Zadace.push({Predmet : data.ispiti[i].naziv, Bodovi : data.ispiti[i].brojBodova})
-                                    }
+
+                        db.sequelize.query("SELECT AkademskaGodina.naziv FROM Predmet, predmet_student, AkademskaGodina WHERE predmet_student.idStudent=" + student_id + " AND Predmet.id=predmet_student.idPredmet AND predmet_student.idAkademskaGodina=AkademskaGodina.id AND Predmet.naziv = '" + data.ispiti[i].naziv + "'").then(([godina, metadata]) => {
+                            //t pomocna varijabla koja govori da li se akademska godina vec nalazi u finalnom nizu
+                            t = false;
+                            i = pom;
+
+                            for (var k = 0; k < niz.length; k++) {
+                                // ako se vec nalazi u finalnom nizu updateuje se samo atribut zadace sa novim predmetom
+                                if (niz[k].AkademskaGodina == godina[0].naziv) {
+                                    t = true;
+                                    niz[k].Zadace.push({ Predmet: data.ispiti[i].naziv, Bodovi: data.ispiti[i].brojBodova })
                                 }
-                                // ako se ne nalazi u finalnom nizu, dodaje se 
-                                if(t == false) {
-                                    niz.push({
-                                        AkademskaGodina : godine[0].naziv,
-                                        Zadace : {Predmet : data.ispiti[i].naziv, Bodovi : data.ispiti[i].brojBodova}
-                                    });
-                                }
-                            });
-                        }
-                    if(data.ispiti.length - 1 == i) {
-                        //nakon sto se prodje kroz zadnji predmet u listi vraca se json
-                        return res.status(200).send({
-                            succes : 'true',
-                            message : 'Succesful',
-                            ZadacePoGodinamaIPredmetima : niz
-                        })
+                            }
+
+                            // ako se ne nalazi u finalnom nizu, dodaje se 
+
+                            if (t == false) {
+
+
+                                niz.push({
+                                    AkademskaGodina: godina[0].naziv,
+                                    Zadace: { Predmet: data.ispiti[i].naziv, Bodovi: data.ispiti[i].brojBodova }
+                                });
+
+                            }
+                            if (data.ispiti.length - 1 == i) {
+                                //nakon sto se prodje kroz zadnji predmet u listi vraca se json
+                                res1.status(200).json({
+                                    succes: 'true',
+                                    message: 'Succesful',
+                                    ZadacePoGodinamaIPredmetima: niz
+                                })
+                            }
+                        });
                     }
+
                 }
             });
         }
@@ -107,32 +124,32 @@ router.get('/:idStudent/sort', function (req, res) {
         }
         else {
             db.sequelize.query("SELECT Predmet.naziv, student_zadatak.brojOstvarenihBodova FROM student_zadatak, Zadatak, Zadaca, Predmet WHERE student_zadatak.idStudent=" + student_id + " AND Zadatak.idZadatak = student_zadatak.idZadatak AND Zadaca.idZadaca = Zadatak.idZadaca AND Predmet.id = Zadaca.idPredmet").then(([zadace, metadata]) => {
-                if(zadace.length == 0) {
+                if (zadace.length == 0) {
                     return res.status(200).send({
-                        succes : 'true',
-                        message : 'Jos nema podataka o zadacama studenta',
+                        succes: 'true',
+                        message: 'Jos nema podataka o zadacama studenta',
                     });
                 }
-                for(var i = 0; i < zadace.length; i++) {
+                for (var i = 0; i < zadace.length; i++) {
                     p = false;
-                    for(var j = 0; j < predmeti.length; j++) {
-                        if(zadace[i].naziv == predmeti[j].naziv) {
+                    for (var j = 0; j < predmeti.length; j++) {
+                        if (zadace[i].naziv == predmeti[j].naziv) {
                             predmeti[j].brojBodova += zadace[i].brojOstvarenihBodova;
                             p = true;
                         }
                     }
-                    if(p == false) {
+                    if (p == false) {
                         predmeti.push({
-                            naziv : zadace[i].naziv,
-                            brojBodova  : zadace[i].brojOstvarenihBodova
+                            naziv: zadace[i].naziv,
+                            brojBodova: zadace[i].brojOstvarenihBodova
                         });
                     }
-                    if(i == zadace.length - 1) {
+                    if (i == zadace.length - 1) {
                         predmeti.sort((a, b) => { a.brojBodova > b.brojBodova ? 1 : 0 })
                         return res.status(200).send({
-                            succes : 'true',
-                            message : 'Succesful',
-                            ispiti : predmeti
+                            succes: 'true',
+                            message: 'Succesful',
+                            ispiti: predmeti
                         });
                     }
                 }
@@ -140,7 +157,7 @@ router.get('/:idStudent/sort', function (req, res) {
             });
         }
     });
-}); 
+});
 
 
 module.exports = router;
