4cea77ae552293b60584043b7300476ac7f55df8
Lejla Kasum <lkasum1@etf.unsa.ba>
2019-06-16 12:05:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ db.sequelize.sync()
 //Setovanje header-a
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
